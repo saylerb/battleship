c853dfc42d06618502330e501c15553b41b2d680
saylerb <sayler.b@gmail.com>
2016-04-09 14:35:33 -0600

    fix game logic

--- lib/game.rb
+++ lib/game.rb
@@ -9,9 +9,7 @@ class Game
     @game_over = false
     @computer_board = Board.new
     @human_board = Board.new
-
     @random_generator = Random.new
-
   end
 
   def play
@@ -19,45 +17,10 @@ class Game
     @game_over = false
 
     set_computer_board
-
-    puts Print.ship_placement_instructions
-
-    print "enter x1: "
-    x1 = gets.chomp.to_i
-    print "enter y1: "
-    y1 = gets.chomp.to_i
-
-    @human_board.add_ship([x1,y1])
-
-    print "enter x2: "
-    x2 = gets.chomp.to_i
-    print "enter y2: "
-    y2 = gets.chomp.to_i
-
-    if (x1 != x2) || (y1 != y2)
-        human_valid = true
-    end
-    @human_board.add_ship([x2,y2])
-
-    puts "boards are set!(?) ready to play!"
+    set_human_board
 
     until @game_over
-
-      puts "COMPUTER:"
-      @computer_board.display_grid
-      puts "HUMAN:"
-      @human_board.display_grid
-      # user_input = gets.chomp
-      # check_user_input(user_input)
-
-      print "Enter a x: "
-      user_input_x = gets.chomp
-
-      print "Enter a y: "
-      user_input_y = gets.chomp
-
-      puts "TODO: check computer board for hits"
-
+      guess_sequence
       if all_my_ships_are_belong_to_computer
         @game_over = true
       elsif all_computer_ships_are_belong_to_human
@@ -67,8 +30,6 @@ class Game
       end
     end
   end
-
-
 
   def set_computer_board
     comp_x1 = @random_generator.rand(4)
@@ -86,6 +47,44 @@ class Game
         valid = true
       end
     end
+    puts Print.ship_placement_instructions
   end
 
+  def set_human_board
+    print "enter x1: "
+    x1 = gets.chomp.to_i
+    print "enter y1: "
+    y1 = gets.chomp.to_i
+
+    @human_board.add_ship([x1,y1])
+
+    print "enter x2: "
+    x2 = gets.chomp.to_i
+    print "enter y2: "
+    y2 = gets.chomp.to_i
+
+    if (x1 != x2) || (y1 != y2)
+        human_valid = true
+    end
+    @human_board.add_ship([x2,y2])
+    puts "boards are set!(?) ready to play!"
+  end
+
+  def guess_sequence
+
+    puts "COMPUTER:"
+    @computer_board.display_grid
+    puts "HUMAN:"
+    @human_board.display_grid
+    # user_input = gets.chomp
+    # check_user_input(user_input)
+
+    print "Enter a x: "
+    user_input_x = gets.chomp
+    print "Enter a y: "
+    user_input_y = gets.chomp
+
+    puts "TODO: check computer board for hits"
+
+  end
 end
